6fbce0947b762a449a33cb12fc6f6b43b072e3db
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2023-07-19 23:56:43 +0530

    Add files via upload

---
+++ Linked_List/Node.java
@@ -0,0 +1,40 @@
+//package Aapna_College_DSA.Linked_List;
+//
+//import java.util.*;
+//
+//class LL{
+//    Node head;
+//    private int size = 0;
+//
+//}
+//
+//public class Node {
+//    String data;
+//    Node next;
+//    int size;
+//
+//    Node(String data) {
+//        this.data = data;
+//        this.next = null;
+//        size++;
+//    }
+//
+////  add - First , last
+//public void addFirst(String data){
+//    Node newNode  = new Node(data);
+//    Node head;
+//    newNode.next = head;
+//    head = newNode;
+//}
+//
+//
+//
+//    public static void main(String[] args) {
+//        LL list = new LL();
+//        list.addLast("is");
+//        list.addLast("a");
+//        list.addLast("list");
+//        list.printList();
+//    }
+//}
+//
