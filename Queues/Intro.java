eadd78316f298a75a6e3d5c6c78730f027bc1219
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2023-07-27 23:19:30 +0530

    Add files via upload

---
+++ Queues/Intro.java
@@ -0,0 +1,25 @@
+package Aapna_College_DSA.Queues;
+
+public class Intro{
+    /*
+    FIFO
+
+      Queue Implement in three ways
+    1. Array        --> Size is static ( less use to implement stack --> O(n) )
+    2. ArrayList    --> Size is Dynamic --> O(1)
+    3. LinkedList   --> Size is Dynamic --> O(1)
+
+      Operations, Circular Queue using Array and Comp
+    1. Enque    --> Add       --> O(1)
+    2. Dequeue  --> Remove    --> O(1)
+    3. Front    --> Peek      --> O(1)
+
+       Operations, Queue using Array and Comp
+    1. Enque    --> Add       --> O(1)
+    2. Dequeue  --> Remove      --> O( n )
+    3. Front    --> Peek        --> O( n )
+
+
+
+     */
+}
---
+++ Queues/Queue_Collection_Frameworks.java
@@ -0,0 +1,4 @@
+package Aapna_College_DSA.Queues;
+
+public class Queue_Collection_Frameworks {
+}
