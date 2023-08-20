cac433545831f634ca4cfd655a18260a0045743b
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2023-08-21 02:55:11 +0530

    Update Half_Pramid_4.java

--- Patterns_Qustio/Half_Pramid_4.java
+++ Patterns_Qustio/Half_Pramid_4.java
@@ -9,7 +9,7 @@ public class Half_Pramid_4 {
 //            System.out.println("  ");
 //        }
 
-               //    OR
+        //    OR
 
         for( int i=4;i>=1;i--){
             for( int j =1; j<=i;j++){
