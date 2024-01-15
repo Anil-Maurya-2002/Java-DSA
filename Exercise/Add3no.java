68146059f33f64607cfc8c42067f154252f7a7da
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2024-01-15 18:27:34 +0530

    Add files via upload

---
+++ Exercise/Add3no.java
@@ -0,0 +1,13 @@
+class Add3no{
+        public static void main(String[] args) {
+                System.out.println("Add 3 no =");
+                int num1 = 5;
+                int num2 = 6;
+                int num3 = 7;
+                int sum;
+                sum = num1+ num2+ num3;
+                // int sum;
+                System.out.println(sum);
+                
+        }
+}
