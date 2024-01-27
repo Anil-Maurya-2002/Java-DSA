00bf0e94e9280012bf3f1097b272c06a0c2bae13
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2024-01-27 16:55:03 +0530

    Add files via upload

---
+++ OOPs/Methad/Volume.java
@@ -0,0 +1,15 @@
+class VolumeBox{
+        double width;
+        double height;
+        double depth;
+        void Volume(){
+                System.out.println("Volume is");
+                System.out.println(width*depth*height);
+        }
+}
+class Volume{
+public static void name(String args[]) {
+VolumeBox Box1 = new VolumeBox()  
+     
+}
+   
